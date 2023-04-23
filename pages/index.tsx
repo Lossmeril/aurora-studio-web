d2c981d40b7f70383fec97269f88ec45b176259b
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2023-04-23 20:54:04 +0100

    fix: feature daily cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Card, Col, Grid, Text } from "@nextui-org/react";
+import { Text } from "@nextui-org/react";
 
 //COMPONENTS
 import Glow from "@/components/Glow";
@@ -23,11 +23,8 @@ export default function Home() {
         <div className="header">
           <div className="subtitle">
             <Text
-              h2
               size={80}
               css={{
-                textGradient: "45deg, $blue600 -20%, $pink600 50%",
-                mixBlendMode: "exclusion",
                 lineHeight: "1em",
               }}
               weight="bold"
@@ -38,35 +35,8 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="works">
-          <Grid.Container gap={2} justify="center">
-            <Grid xs={12} sm={2}>
-              <Card>
-                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-                  <Col>
-                    <Text
-                      size={12}
-                      weight="bold"
-                      transform="uppercase"
-                      color="#ffffffAA"
-                    >
-                      What to watch
-                    </Text>
-                    <Text h4 color="white">
-                      Stream the Acme event
-                    </Text>
-                  </Col>
-                </Card.Header>
-                <Card.Image
-                  src="https://nextui.org/images/card-example-4.jpeg"
-                  objectFit="cover"
-                  width="100%"
-                  height={340}
-                  alt="Card image background"
-                />
-              </Card>
-            </Grid>
-          </Grid.Container>
+        <div className="works section">
+          <Text h2>Naše projekty</Text>
         </div>
       </main>
     </>
